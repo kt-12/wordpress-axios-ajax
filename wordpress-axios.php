0d6c518ef044f79c313c56ee634dd1f83403c978
Karthik Thayyil <30643833+kt-12@users.noreply.github.com>
2017-10-10 08:44:02 +0530

    Typos and Enhancements

--- wordpress-axios.php
+++ wordpress-axios.php
@@ -2,8 +2,8 @@
 
 /**
  * This is of simple demonstration of using Axios (https://github.com/axios/axios) for making WordPress Axaj call
- * The Ajax call will fetch a simple randomly generated name array from the backend and display it onto the screen.
- * The display logic is written using Vue.js 2.
+ * The ajax call will fetch a randomly generated name array from the backend and display it onto the screen.
+ * The display logic is written using Vue.js v2.
  *
  *  @package wordpress-axios-ajax
  *
@@ -11,38 +11,40 @@
  * Description:       The plugin will demonstrate a simple WordPress ajax call using axios. the display logic is written using Vue.js
  * Version:           1.0.0
  * Author:            Karthik Thayyil
- * Author URI:        https://kt12.in/
+ * Author URI:        https://kt12.in/blog/wordpress-ajax-call-using-axios-js/
  */
 
 // If this file is called directly, abort.
-if (!defined('WPINC')) {
+if ( !defined('WPINC') ) {
     die;
 }
 
 /**
  *  Enqueue all scripts needed for this demo
- * @return void
+ *  @return void
  */
 function wordpress_axios_scripts()
 {
     // it's a good practice to download the files locally.
-    wp_enqueue_script('WAS-vue-js', 'https://unpkg.com/vue@2.4.4/dist/vue.js', array(), '2.4.4', true);
-    wp_enqueue_script('WAS-axios-js', 'https://unpkg.com/axios@0.16.2/dist/axios.min.js', array(), '0.16.2', true);
-    wp_enqueue_script('WAS-qs-js', 'https://unpkg.com/qs@6.5.1/dist/qs.js', array(), '6.5.1', true);
+    wp_enqueue_script( 'WAS-vue-js', 'https://unpkg.com/vue@2.4.4/dist/vue.js', array(), '2.4.4', true );
+    wp_enqueue_script( 'WAS-axios-js', 'https://unpkg.com/axios@0.16.2/dist/axios.min.js', array(), '0.16.2', true );
+    wp_enqueue_script( 'WAS-qs-js', 'https://unpkg.com/qs@6.5.1/dist/qs.js', array(), '6.5.1', true );
 
     // this script will display AJAX response using Vue
-    wp_enqueue_script('WAS-functionality-js', plugin_dir_url(__FILE__).'js/custom.js', array( 'WAS-axios-js', 'WAS-vue-js', 'WAS-qs-js' ), '1.0.0', true);
+    wp_enqueue_script( 'WAS-functionality-js', plugin_dir_url( __FILE__ ).'js/custom.js', array( 'WAS-axios-js', 'WAS-vue-js', 'WAS-qs-js' ), '1.0.0', true );
 
     wp_localize_script(
                 'WAS-functionality-js',
                 'WAS_ajax_obj',
-                array('ajax_url'=>admin_url('admin-ajax.php'))
+                array( 'ajax_url' => admin_url( 'admin-ajax.php' ) )
             );
 }
-add_action('wp_enqueue_scripts', 'wordpress_axios_scripts');
+
+add_action( 'wp_enqueue_scripts', 'wordpress_axios_scripts' );
 
 /*
  * Vue structure which will be embedded in the footer area
+ * @return void
  */
 function vue_demo_structure()
 {
@@ -56,18 +58,21 @@ function vue_demo_structure()
     </ul>
 <?php  echo ob_get_clean();
 }
-add_action('wp_footer', 'vue_demo_structure');
+
+add_action( 'wp_footer', 'vue_demo_structure' );
 
 
 /**
- * Ajax Code That will return a array of random names.
+ * ajax code that will return an array of random names.
+ * @return void
  */
 function ajaxReturnRandomNames()
 {
-    $names = array("Millie Mathews", "Susana Sampley", "Moshe Pablo", "Claire Pakele", "Linwood Mayr", "Sarina Lundin", "Hugh Driggs", "Jade Krom", "Lina Pavia", "Gordon Stutes", "Lucila Erickson", "Deonna Cheatham", "Anh Mullins", "Kathlyn Manfre", "Flor Maguire", "Syreeta Mosqueda", "Fermina Dziedzic", "Jeff Soule", "Felton Gargiulo", "Hilary Varian");
-    shuffle($names);
-    $rand_names = array_slice($names, 0, rand(1, 20));
-    wp_send_json($rand_names);
+    $names = array( "Millie Mathews", "Susana Sampley", "Moshe Pablo", "Claire Pakele", "Linwood Mayr", "Sarina Lundin", "Hugh Driggs", "Jade Krom", "Lina Pavia", "Gordon Stutes", "Lucila Erickson", "Deonna Cheatham", "Anh Mullins", "Kathlyn Manfre", "Flor Maguire", "Syreeta Mosqueda", "Fermina Dziedzic", "Jeff Soule", "Felton Gargiulo", "Hilary Varian" );
+    shuffle( $names );
+    $rand_names = array_slice( $names, 0, rand( 1, 20 ) );
+    wp_send_json( $rand_names );
 }
-add_action('wp_ajax_nopriv_was_get_names', 'ajaxReturnRandomNames');  //handles non logged in user ajax calls
-add_action('wp_ajax_was_get_names', 'ajaxReturnRandomNames');
+
+add_action( 'wp_ajax_nopriv_was_get_names', 'ajaxReturnRandomNames' );  //handles non logged in user ajax calls
+add_action( 'wp_ajax_was_get_names', 'ajaxReturnRandomNames' );
